da6395494074cc2815e2e914cd37d2c9e4230554
BUZZINGPolarBear <jjh63360@kau.kr>
2021-11-04 23:04:52 +0900

    Add main screen

---
+++ src/GUI.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import javax.swing.*;
+import java.util.Random;
+import java.util.Scanner;
+
+public class GUI extends JFrame
+{
+    public void mainScreen()
+    {
+        setVisible(true);
+        setSize(1200, 920);
+        setTitle("DB후반기 5조 팀플");
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+}
